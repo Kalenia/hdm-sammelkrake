4373c479d0b9f398c50d3a9b2b0773129f99110a
Nina Kast <nk068@hdm-stuttgart.de>
2017-05-26 15:31:30 +0200

    Kalender in Kachel eingebunden.

--- public/index.php
+++ public/index.php
@@ -16,14 +16,13 @@ require_once(ROOT_PATH . '/include/view_helpers.php');
 <head>
     <meta charset="UTF-8">
     <title>HdM Sammelkrake</title>
-    <link rel="stylesheet" href="css/ExperimentKachelnCss.css">
+    <link rel="stylesheet" href="style/styleTest.css">
 </head>
 <body>
 
     <div class="wrapper">
         <aside>
             <ul>
-                <img class="menubild" src="img/images.jpg" alt="menubild">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#news">News</a></li>
                 <li><a href="#contact">Contact</a></li>
@@ -31,36 +30,64 @@ require_once(ROOT_PATH . '/include/view_helpers.php');
             </ul>
         </aside>
 
-
+            <div class="offWorkingStuff">
+                <span class="title"> Offizielles Arbeitsmaterial </span>
+                <div> Lorem Ipsum dolor set ....... </div>
+            </div>
             <div class="officialNews">
                 <span class="title"> Offizielle News </span>
                 <div> Lorem Ipsum dolor set ....... </div>
             </div>
-            <div class="kennstDuSchon">
-                <span class="title"> Kennst du schon ... ? </span>
-                <div> Lorem Ipsum dolor set ....... </div>
+            <div class="studentsStuff">
+            <span class="title"> Deine Informationen </span>
+            <div> Lorem Ipsum dolor set ....... </div>
             </div>
+
             <div class="ownCloud">
                 <span class="title"> OwnCloud </span>
                 <div> Lorem Ipsum dolor set ....... </div>
             </div>
-            <div class="sBar">
-                <span class="title"> Speiseplan der S Bar </span>
-                <div> Lorem Ipsum dolor set ....... </div>
-            </div>
             <div class="schedule">
                 <span class="title"> Dein persönlicher Stundenplan </span>
+                <div>
+                    <?php	include('/tiles/02-schedule.php') ?>
+                </div>
+            </div>
+
+            <div class="kennstDuSchon">
+                <span class="title"> Kennst du schon ... ? </span>
+                <div id="picture">
+
+                </div>
+                <div id="name">
+                    Ludwig Kösling
+                </div>
+            </div>
+            <div class="kurzelKai">
+                <span class="title"> Kürzel Kai </span>
                 <div> Lorem Ipsum dolor set ....... </div>
             </div>
-            <div class="studentsStuff">
-                <span class="title"> Deine Informationen </span>
+            <div class="importantNews">
+                <span class="title"> Wichtige Meldungen </span>
                 <div> Lorem Ipsum dolor set ....... </div>
             </div>
 
+        <div class="sbar">
+            <span class="title"> S Bar Menü </span>
+            <div> Lorem Ipsum dolor set ....... </div>
+        </div>
+        <div class="mensa">
+            <span class="title"> Mensa Menü </span>
+            <div> Lorem Ipsum dolor set ....... </div>
+        </div>
+        <div class="calendar">
+            <span class="title"> Kalender </span>
+            <div>
+            <?php include('11-Calendar.html'); ?>
+             </div>
+        </div>
 
     </div>
-
-
 
 </body>
 </html>
---
+++ public/index2.php
@@ -0,0 +1,55 @@
+<?php
+if ( ! isset($_SERVER['PHP_AUTH_USER']) ) {
+	header('WWW-Authenticate: Basic realm="Sammelkrake"');
+	header('HTTP/1.0 401 Unauthorized');
+	die("Bitte mit deinem HdM-Account anmelden");
+}
+const ROOT_PATH = '..';
+require_once(ROOT_PATH . '/include/view_helpers.php');
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<title>HdM Sammelkrake</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<script src="scripts/jquery.js"></script>
+	<script src="scripts/jquery.grid.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(window).resize(function(){
+				$('section').grid({ 'cell-width': 145, 'cell-height': 200, 'cell-spacing': 10 })//.triggerHandler('debug');
+			});
+			$(window).resize();
+		});
+	</script>
+</head>
+<body>
+
+<header>
+	<h1><a href="/">HdM Sammelkrake</a></h1>
+	<p>Eine kleine Karte der Informationsquellen rund um die HdM</p>
+
+	<ul id="legend">
+		<li class="official changing" title="Teilweise tägliche Meldungen aus offiziellen Informationskanälen der HdM"><span>Aktuelle offizielle Infos</span></li>
+		<li class="official" title="Informationsquellen für dein Studium"><span>Offizielle Informationsquellen</span></li>
+		<li class="social"><span>Soziales</span></li>
+		<!--<li class="projects" title="z.B. Projekte"><span>Eigene Aktivitäten</span></li>-->
+		<li class="misc"><span>Sonstiges</span></li>
+	</ul>
+</header>
+
+<section id="tiles">
+
+<?php foreach( glob('../tiles/*.php') as $tile ): ?>
+<?php	include($tile) ?>
+<?php endforeach ?>
+
+</section>
+
+<div id="details" class="inactive">
+	<article></article>
+</div>
+
+</body>
+</html>
