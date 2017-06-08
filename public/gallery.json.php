aa506199ff270ee5397084eff2adb2ee8edb4b96
Nina Kast <nk068@hdm-stuttgart.de>
2017-06-08 18:55:59 +0200

    Semester Galerie

--- public/gallery.json.php
+++ public/gallery.json.php
@@ -19,23 +19,17 @@ $context = stream_context_create(array(
 	)
 ));
 
-if ( !isset($_GET['semester']) ) {
-	$html_source = file_get_contents("https://www.hdm-stuttgart.de/mm/student/studienanfaenger/", false, $context);
-	
-	$doc = @DOMDocument::loadHTML($html_source);
-	$xpath = new DOMXPath($doc);
-	$semester_list_node = $xpath->query("//select[@name='directory']")->item(0);
-	$semester_list = simplexml_import_dom($semester_list_node);
-	
-	$semester_list_object = [];
-	foreach($semester_list->option as $option) {
-		$id = strval($option['value']);
-		$display_name = strval($option);
-		$semester_list_object[$id] = $display_name;
-	}
-	
-	die(json_encode($semester_list_object));
-} else {
+if ( isset($_GET['image']) ) {
+    // Request an image in the name of the client
+    $path = urldecode($_GET['image']);
+    $from_mi_server = fopen("https://www.hdm-stuttgart.de$path", "rb", false, $context);
+    $to_client = fopen("php://output", "wb");
+    stream_copy_to_stream($from_mi_server, $to_client);
+    fclose($to_client);
+    fclose($from_mi_server);
+    exit();
+} else if ( isset($_GET['semester']) ) {
+    // Request image list of that semester
 	$escaped_id = urlencode($_GET['semester']);
 	$url = "https://www.hdm-stuttgart.de/mm/student/studienanfaenger/?directory=$escaped_id&Anzeige=Anzeigen";
 	$html_source = file_get_contents($url, false, $context);
@@ -49,10 +43,26 @@ if ( !isset($_GET['semester']) ) {
 		$student_node = $gallery->item($i);
 		$name = $xpath->evaluate("string(.)", $student_node);
 		$image_path = $xpath->evaluate("string(.//img/@src)", $student_node);
-		$image_url = "https://www.hdm-stuttgart.de/$image_path";
+		$image_url = "$image_path";
 		
 		$gallery_object[trim($name)] = trim($image_url);
 	}
 	
 	die(json_encode($gallery_object));
+} else {
+	$html_source = file_get_contents("https://www.hdm-stuttgart.de/mm/student/studienanfaenger/", false, $context);
+
+	$doc = @DOMDocument::loadHTML($html_source);
+	$xpath = new DOMXPath($doc);
+	$semester_list_node = $xpath->query("//select[@name='directory']")->item(0);
+	$semester_list = simplexml_import_dom($semester_list_node);
+
+	$semester_list_object = [];
+	foreach($semester_list->option as $option) {
+		$id = strval($option['value']);
+		$display_name = strval($option);
+		$semester_list_object[$id] = $display_name;
+	}
+
+	die(json_encode($semester_list_object));
 }
