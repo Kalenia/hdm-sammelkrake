b0d33a5120fef1eeacd15324fbcce6e2d4d096dd
Kevin Vetter <kv014@hdm-stuttgart.de>
2017-06-29 15:07:54 +0200

    medianight mockup

--- public/index.php
+++ public/index.php
@@ -21,36 +21,49 @@ require_once(ROOT_PATH . '/include/view_helpers.php');
 
     <link rel="stylesheet" href="../style/Calendar.css" media="screen">
 
+    <link rel="stylesheet" href="../style/cardcss.css">
+
     <script src="scripts/jquery.js"></script>
     	<script src="scripts/jquery.grid.js"></script>
 
 </head>
 <body>
 
+
+
     <div class="wrapper">
 
+
+
             <article class="offWorkingStuff">
-                <span class="title"> Offizielles Arbeitsmaterial </span>
-                <div>
-                    <?php include("../tiles/ideas/03-official-working-stuff.php")?>
-                </div>
+                             <div class="card effect__click">
+                               <div class="card__front">
+                               </div>
+                               <div class="card__back">
+                                 <span class="card__text"> <?php include("../tiles/ideas/03-official-working-stuff.php")?></span>
+                               </div>
+                             </div><!-- /card -->
+
             </article>
+
             <article class="officialNews">
-                <span class="title"> Offizielle News </span>
-                <div>
+
+              <div>
                     <?php include("../tiles/ideas/04-official-infos.php")?>
                 </div>
+
             </article>
+
             <article class="studentsStuff">
-            <span class="title"> Deine Informationen </span>
+            <span class="title">  </span>
             <div>
                 <?php include("../tiles/ideas/06-students-stuff.php")?>
             </div>
             </article>
 
             <article class="ownCloud">
-                <span class="title"> OwnCloud </span>
-                <div> Lorem Ipsum dolor set ....... </div>
+                <span class="title"> </span>
+                <div> </div>
             </article>
             <article class="schedule">
 
@@ -62,15 +75,17 @@ require_once(ROOT_PATH . '/include/view_helpers.php');
             <article class="kennstDuSchon">
                 <span class="title"> Kennst du schon ... ? </span>
                 <div id="picture">
-                     <?php include("../tiles/semGall.html")?>
+                    <?#php include("../tiles/semGall.html")?>
                 </div>
             </article>
+
             <article class="kurzelKai">
                 <span class="title"> Kürzel Kai </span>
                 <div>
                 <?php include("../tiles/06-kürzelkai.php")?>
                 </div>
             </article>
+
             <article class="importantNews">
                 <span class="title"> Wichtige Meldungen </span>
                 <div>
@@ -79,36 +94,44 @@ require_once(ROOT_PATH . '/include/view_helpers.php');
             </article>
 
         <article class="sbar">
-            <span class="title"> S Bar Menü </span>
+            <span class="title"> </span>
             <div>
 
             </div>
         </article>
+
         <article class="mensa">
-            <span class="title"> Mensa Menü </span>
-            <div> Lorem Ipsum dolor set ....... </div>
+            <span class="title"></span>
+            <div> </div>
         </article>
+
         <article class="calendar">
              <div id="calendar"></div>
         </article>
 
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-    <script src="../scripts/jquery-ui-datepicker.min.js"></script>
 
-    <script>
-        $('#calendar').datepicker({
-            inline: true,
-            firstDay: 1,
-            showOtherMonths: true,
-            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-        });
-    </script>
 
         <script src="foundation-6/js/vendor/jquery.js"></script>
         <script src="foundation-6/js/vendor/what-input.js"></script>
         <script src="foundation-6/js/vendor/foundation.js"></script>
         <script src="foundation-6/js/app.js"></script>
+        <script src="scripts/flip.js"></script>
+
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+            <script src="../scripts/jquery-ui-datepicker.min.js"></script>
+
+
+
+            <script>
+                $('#calendar').datepicker({
+                    inline: true,
+                    firstDay: 1,
+                    showOtherMonths: true,
+                    dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+                });
+            </script>
+
 </body>
 </html>
--- tiles/ideas/03-official-working-stuff.php
+++ tiles/ideas/03-official-working-stuff.php
@@ -1,5 +1,5 @@
-<article id="official-working-stuff" class="official" data-width="2" data-height="1">
-	<h2>Offizielles Arbeitsmaterial</h2>
+<article id="official-working-stuff" class="official-working" data-width="2" data-height="1">
+	<h2 class="test">Offizielles Arbeitsmaterial</h2>
 	<ul>
 		<li><a href="#">MI-Wiki</a> <small>Infos zum Studium, Vorlesungen und HowTos (z.B. VPN)</small></li>
 		<li><a href="#">Skripte-Server</a> <small>Skripte und Daten zu Vorlesungen</small></li>
